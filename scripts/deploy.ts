2e39b5ce0a3b3aa1a69eb5d8741a642f9464a95d
Dennison Bertram <dennison@dennisonbertram.com>
2021-03-21 14:26:13 -0400

    👌 IMPROVE: adding some feature

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,6 +5,12 @@
 import { ethers } from "hardhat";
 import { Contract, ContractFactory } from "ethers";
 
+import Transport from "@ledgerhq/hw-transport-node-hid";
+import Eth from "@ledgerhq/hw-app-eth";
+
+
+
+
 async function main(): Promise<void> {
   // Hardhat always runs the compile task when running scripts through it.
   // If this runs in a standalone fashion you may want to call compile manually
@@ -12,11 +18,22 @@ async function main(): Promise<void> {
   // await run("compile");
 
   // We get the contract to deploy
-  const Greeter: ContractFactory = await ethers.getContractFactory("Greeter");
-  const greeter: Contract = await Greeter.deploy("Hello, Buidler!");
-  await greeter.deployed();
+  // const Greeter: ContractFactory = await ethers.getContractFactory("Greeter");
+  // const greeter: Contract = await Greeter.deploy("Hello, Buidler!");
+  // await greeter.deployed();
 
-  console.log("Greeter deployed to: ", greeter.address);
+  // console.log("Greeter deployed to: ", greeter.address);
+  const getETHAddress = async () => {
+    console.log("HERE")
+    const transport = await Transport.create();
+    transport.setDebugMode(true);
+    const eth = new Eth(transport)
+    console.log("ETH: ", eth);
+    eth.getAddress("44'/60'/0'/0/0").then((o: any) => console.log("Address: ", o.address))
+  }
+  
+  getETHAddress();
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
